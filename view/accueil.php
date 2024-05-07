16421c2466e85bed8ca09b6053507aba18f79243
chaima garoui <chaimagaroui2@gmail.com>
2024-05-07 16:56:34 +0200

    ajout : css template et debut page accueil

--- view/accueil.php
+++ view/accueil.php
@@ -3,33 +3,82 @@ ob_start();
 
 $nvFilms = $requete_nouveaute->fetchAll();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/accueil.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-<link rel="stylesheet" href="css/accueil.css">
+    <title>Document</title>
+</head>
+<body>
+    
 
-<p>Nouveauté</p>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<table>
-    <thead>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="img/jumanji.jpg" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/titanic.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="img/spiderman.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+
+    <p>Nouveauté</p>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Titre </th>
+                <th>ANNEE SORTIE</th>
+                <th>affiche</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach($nvFilms as $nvFilm){ ?>
         <tr>
-            <th>Titre </th>
-            <th>ANNEE SORTIE</th>
-            <th>affiche</th>
+            <td><?=$nvFilm["titre"]?></td>
+            <td><?=$nvFilm["annee_sortie"]?></td>
+            <td><?=$nvFilm["affiche"]?></td>
         </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach($nvFilms as $nvFilm){ ?>
-    <tr>
-        <td><?=$nvFilm["titre"]?></td>
-        <td><?=$nvFilm["annee_sortie"]?></td>
-        <td><?=$nvFilm["affiche"]?></td>
-    </tr>
 
-<?php }?>
-</tbody>
-</table>
+    <?php }?>
+    </tbody>
+    </table>
+
+    </div>
+</body>
 
 
+</html>
 
 
 
